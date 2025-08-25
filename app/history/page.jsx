dad00816dc7527dd9da4b961aa88e2a53104aaa5
AnhHaoGit <nguyenanhhao090708@gmail.com>
2025-08-25 11:23:48 +0700

    adjusting for deployment

---
+++ app/history/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import MainNavbar from "@/components/MainNavbar";
+
+const Page = () => {
+  return (
+    <>
+      <MainNavbar />
+      <div>History Page</div>
+    </>
+  );
+};
+
+export default Page;
