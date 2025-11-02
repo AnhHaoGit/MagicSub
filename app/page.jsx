a036991fcaa8f988645bcfa133d53b2d73e9d787
Anh Hao Nguyen <nguyenanhhao090708@gmail.com>
2025-11-02 11:37:21 +0700

    Remove Product Hunt badge from page
    Removed Product Hunt badge link and image from the page.

--- app/page.jsx
+++ app/page.jsx
@@ -66,20 +66,6 @@ const LandingPage = () => {
           what’s going on.
         </p>
 
-        <a
-          href="https://www.producthunt.com/products/magicsub?embed=true&utm_source=badge-featured&utm_medium=badge&utm_source=badge-magicsub"
-          target="_blank"
-          className="mt-6"
-        >
-          <Image
-            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1033287&theme=neutral&t=1762004069400"
-            alt="MagicSub - Wave&#0032;goodbye&#0032;to&#0032;&#0034;can&#0032;someone&#0032;explain&#0034;&#0032;comments&#0046; | Product Hunt"
-            style="width: 200px; height: 27px;"
-            width="200"
-            height="27"
-          />
-        </a>
-
         {/* Upload Section */}
         {session && status === "authenticated" ? (
           <Link
